3b7e59f969af9d4587e75b12d38df81dbba4d67c
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-10-24 00:52:34 +0600

    stylesheet

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <div className="Title">
+        <p>AJOOBA TRIVIA</p>
+        <p><span>stay tuned at</span><br></br><span>@ajoobatrivia</span></p>
+      </div>
     </div>
   );
 }
